3f5b066ddff4ed0b74b0cd6123d4afed0a749667
Pranav Singh <pranavvalt@gmail.com>
2024-01-27 16:22:53 +0530

    final

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -30,17 +30,20 @@ const Row = ({collection_name}: {collection_name: string}) => {
    }, [])
 
    return (
-      <div className="space-y-0.5 md:space-y-2">
+      <div className="space-y-0.5">
          <h2 className="w-56 cursor-pointer text-sm md:text-xl font-semibold text-[#e5e5e5] transition duration-200 hover:text-white capitalize">
             {formatString(collection_name)}
          </h2>
          <div className="group/row relative md:-ml-5">
-            <FaChevronLeft
-               className={`absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover/row:opacity-100 ${
+            <div
+               className={`absolute flex items-center top-0 bottom-0 left-0 z-40 m-auto h-36  opacity-0 transition  group-hover/row:opacity-100 bg-black/[0.5] ${
                   !isMoved && "hidden"
                }`}
                onClick={() => handleClick("left")}
-            />
+            >
+               <FaChevronLeft className="h-9 w-9 cursor-pointer hover:scale-125" />
+            </div>
+
             <div
                ref={rowRef}
                className="scrollbar-hide flex items-center space-x-0.5 overflow-x-scroll md:space-x-2.5 p-5"
@@ -50,7 +53,7 @@ const Row = ({collection_name}: {collection_name: string}) => {
                ))}
             </div>
             <span
-               className="absolute flex items-center top-0 bottom-0 right-0 z-40 m-auto h-36  opacity-0 transition  group-hover/row:opacity-100 bg-black/[0.5] my-auto"
+               className="absolute flex items-center top-0 bottom-0 right-0 z-40 m-auto h-36 opacity-0 transition  group-hover/row:opacity-100 bg-black/[0.5]"
                onClick={() => handleClick("right")}
             >
                <FaChevronRight className="h-9 w-9 cursor-pointer hover:scale-125" />
